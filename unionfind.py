8e889a4ab83a4c60e1a30bfa2745ca8eb8b7d6bd
Seadog1225 <aasssa1225@naver.com>
2020-09-11 23:11:36 +0900

    Practicing Union-Find For Checking Cycle

--- unionfind.py
+++ unionfind.py
@@ -1,15 +1,13 @@
-# union-find ver2
+# union-find 를 이용한 무방향 그래프 사이클 판별
 
-# 특정 원소가 속한 집합을 찾기, 경로 압축 기법
+# 특정 원소가 속한 집합 찾기
 def find(parent, x):
     if parent[x] != x:
         parent[x] = find(parent, parent[x])
     return parent[x]
 
-# 두 원소가 속한 집합을 합치기
 
-
-def union(parent, x, y):
+def union(parent, x, y):  # 두 원소가 속한 집합을 합치기
     a = find(parent, x)
     b = find(parent, y)
     if a > b:
@@ -18,30 +16,70 @@ def union(parent, x, y):
         parent[b] = a
 
 
-# 노드의 개수와 union연산개수(간선의 개수) 입력 받기
+# 노드의 개수와 union 연산개수(간선의 개수) 입력받기
 v, e = map(int, input().split())
-
-# 부모 테이블 초기화
+# 부모테이블 초기화
 parent = [0] * (v + 1)
 
-# 부모 테이블상에서, 부모를 자기 자신으로 초기화
-for i in range(1, v + 1):
+# 부모 테이블 상에서, 부모를 자기 자신으로 초기화
+for i in range(1, v+1):
     parent[i] = i
 
-# union 연산을 각각 수행
+# 사이클 판별
+cycle = False
+
 for _ in range(e):
     a, b = map(int, input().split())
-    union(parent, a, b)
+    if find(parent, a) != find(parent, b):
+        union(parent, a, b)
+    else:
+        cycle = True
+        break
 
-# 각 원소가 속한 집합 출력
-for i in range(1, v + 1):
-    print(find(parent, i), end=" ")
+print(cycle)
 
-print()
+# # union-find ver2
 
-# 부모 테이블 내용 출력
-for i in range(1, v + 1):
-    print(parent[i], end=" ")
+# # 특정 원소가 속한 집합을 찾기, 경로 압축 기법
+# def find(parent, x):
+#     if parent[x] != x:
+#         parent[x] = find(parent, parent[x])
+#     return parent[x]
+
+
+# def union(parent, x, y):  # 두 원소가 속한 집합을 합치기
+#     a = find(parent, x)
+#     b = find(parent, y)
+#     if a > b:
+#         parent[a] = b
+#     else:
+#         parent[b] = a
+
+
+# # 노드의 개수와 union연산개수(간선의 개수) 입력 받기
+# v, e = map(int, input().split())
+
+# # 부모 테이블 초기화
+# parent = [0] * (v + 1)
+
+# # 부모 테이블상에서, 부모를 자기 자신으로 초기화
+# for i in range(1, v + 1):
+#     parent[i] = i
+
+# # union 연산을 각각 수행
+# for _ in range(e):
+#     a, b = map(int, input().split())
+#     union(parent, a, b)
+
+# # 각 원소가 속한 집합 출력
+# for i in range(1, v + 1):
+#     print(find(parent, i), end=" ")
+
+# print()
+
+# # 부모 테이블 내용 출력
+# for i in range(1, v + 1):
+#     print(parent[i], end=" ")
 
 # union-find ver1
 
