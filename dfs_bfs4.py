33b812a733e716ec3870299f777f177635169244
SSeadog <aasssa1225@naver.com>
2020-09-30 21:38:48 +0900

    Practicing DFS BFS

--- dfs_bfs4.py
+++ dfs_bfs4.py
@@ -5,39 +5,60 @@ n, m = map(int, input().split())
 
 # 지도 모양 입력
 graph = []
+# 벽이 설치된 후의 지도. 특정 크기의 2차원 리스트는 리스트 컴프리헨션을 이용해야 함.
+temp = [[0] * m for _ in range(n)]
 for _ in range(n):
     graph.append(list(map(int, input().split())))
 
 
 result = []
 
-test_graph = graph
-
-# 아무 위치에 벽 세우기
-for a_i in range(n):
-    for a_j in range(m):
-        if test_graph[a_i][a_j] != 0:
-            continue
-        test_graph[a_i][a_j] = 1
-        for b_i in range(n):
-            for b_j in range(m):
-                if test_graph[b_i][b_j] != 0:
-                    continue
-                test_graph[b_i][b_j] = 1
-                for c_i in range(n):
-                    for c_j in range(m):
-                        if test_graph[c_i][c_j] != 0:
-                            continue
-                        test_graph[c_i][c_j] = 1
-                        # 바이러스 퍼뜨리기 0 개수 구하기
-                        cnt = 0
-                        # for g in test_graph:
-                        #     for x in g:
-                        #         if x == 0:
-                        #             cnt += 1
-                        # result.append(cnt)
-                        print(test_graph)
-                test_graph = graph
+# 이동 방향에 대한 리스트 상 좌 하 우
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
 
 
-# print(result)
+def virus(x, y):
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if nx >= 0 and nx < n and ny >= 0 and ny < m:
+            if temp[nx][ny] == 0:
+                temp[nx][ny] = 2
+                virus(nx, ny)
+
+
+def get_safe():
+    score = 0
+    for i in range(n):
+        for j in range(m):
+            if temp[i][j] == 0:
+                score += 1
+    return score
+
+
+def dfs(cnt):
+    if cnt == 3:
+        for i in range(n):
+            for j in range(m):
+                temp[i][j] = graph[i][j]
+        # 바이러스 퍼지고 안전 영역 구하기
+        for i in range(n):
+            for j in range(m):
+                if temp[i][j] == 2:
+                    virus(i, j)
+        safe_score = get_safe()
+        result.append(safe_score)
+        return
+    for i in range(n):
+        for j in range(m):
+            if graph[i][j] == 0:
+                graph[i][j] = 1
+                cnt += 1
+                dfs(cnt)
+                graph[i][j] = 0
+                cnt -= 1
+
+
+dfs(0)
+print(max(result))
