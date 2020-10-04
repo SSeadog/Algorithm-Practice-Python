80c9b35310b4716ca28917eac79c6f7466848129
Seadog1225 <aasssa1225@naver.com>
2020-10-04 10:00:57 +0900

    Practicing DFS BFS

--- dfs_bfs6.py
+++ dfs_bfs6.py
@@ -1,7 +1,18 @@
 # 괄호 변환
 
-# 1,2번에 해당하는 함수
-def split(w):
+def is_right(u):  # 올바른 문자열인지 확인
+    cnt_a = 0
+    for i in u:
+        if i == '(':
+            cnt_a += 1
+        else:
+            cnt_a -= 1
+        if cnt_a < 0:
+            return False
+    return True
+
+
+def process(w):  # 재귀적으로 부르기 위해 모든 과정을 한 함수에 모음
     if w == "":
         return ""
     u = ''
@@ -16,32 +27,33 @@ def split(w):
             cnt_b += 1
         if cnt_a == cnt_b:
             break
-    v = w[cnt_a + cnt_b + 1:]
-    return u, v
-
-# 올바른 문자열인지 확인
-
-
-def chk_right(u):
-    cnt_a = 0
-    for i in w:
-        if i == "(":
-            cnt_a += 1
-        else:
-            cnt_a -= 1
-        if cnt_a < 0:
-            return False
-    return True
+    v = w[cnt_a + cnt_b:]
+    print("u", u, "v", v)
+    if chk_right(u):
+        # 3-1
+        u += process(v)
+        return u
+    else:
+        # 4
+        new = '('
+        new += process(v)
+        new += ')'
+        print("u", u, "v", v)
+        print("u", u)
+        for j in u[1:-1]:  # [1:-1]은 1인덱스부터 마지막 인덱스 앞까지만 반복
+            if j == '(':
+                new += ')'
+            else:
+                new += '('
+        return new
 
 
 def solution(p):
-    answer = ''
-    u, v = split(p)
+    answer = process(p)
 
-    # 3번부터 만들면 됨
     return answer
 
 
 p = input()
 
-solution(p)
+print(solution(p))
