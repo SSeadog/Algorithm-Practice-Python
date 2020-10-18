068f4625261457385ad9582b1c82b64cb837e0b9
Seadog1225 <aasssa1225@naver.com>
2020-10-19 00:00:36 +0900

    Practicing Binary Search

---
+++ search/binay_search4.py
@@ -0,0 +1,25 @@
+# 고정점
+
+# 이진 탐색으로 구현
+# 이진 탐색에 대한 이해가 부족함 아직
+
+n = int(input())
+
+array = list(map(int, input().split()))
+
+
+def binary_search(array, start, end):
+    if start > end:
+        return -1
+    mid = (start + end) // 2
+    if array[mid] == mid:
+        return mid
+    elif array[mid] > mid:
+        return binary_search(array, start, mid-1)
+    elif array[mid] < mid:
+        return binary_search(array, mid+1, end)
+
+
+result = binary_search(array, 0, len(array)-1)
+
+print(result)
