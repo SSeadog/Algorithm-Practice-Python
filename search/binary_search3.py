d1956a0657cd9952dea915c4eff358cc60e4daa7
Seadog1225 <aasssa1225@naver.com>
2020-11-23 08:54:39 +0900

    Practicing Binary Search

--- search/binary_search3.py
+++ search/binary_search3.py
@@ -1,32 +1,30 @@
 # 정렬된 배열에서 특정 수의 개수 구하기
 
-def get_start(li, t, start, end):
-    mid = (end - start) // 2
-    print(li[mid - 1], li[mid])
-    if li[mid] == t and (mid == n - 1 or li[mid] != li[mid + 1]):
+def get_first(li, t, start, end):
+    mid = (end + start) // 2
+    # 이 발상이 전혀 가능해 보이지 않는데..
+    if li[mid] == t and (mid == 0 or t > li[mid - 1]):
         return mid
     elif li[mid] >= t:
-        return get_start(li, t, start, mid - 1)
+        return get_first(li, t, start, mid - 1)
     else:
-        return get_start(li, t, mid + 1, end)
-
-
-def get_last(li, t, start, end):
-    mid = (end - start) // 2
-    print(li[mid], li[mid + 1])
-    if li[mid] == t and (mid == 0 or li[mid] != li[mid - 1]):
-        return mid
-    elif li[mid] > t:
-        return get_last(li, t, start, mid - 1)
-    else:
-        return get_last(li, t, mid + 1, end)
+        return get_first(li, t, mid + 1, end)
 
 
 n, x = map(int, input().split())
 
 l = list(map(int, input().split()))
 
-start = get_start(l, x + 1, 0, n)
-end = get_last(l, x - 1, 0, n)
+if x <= l[n - 1]:
+    first = get_first(l, x, 0, n - 1)
+else:
+    first = n - 1
+if x + 1 <= l[n - 1]:
+    last = get_first(l, x + 1, 0, n - 1)
+else:
+    last = n - 1
 
-print(start-end)
+if first == last:
+    print(-1)
+else:
+    print(last - first)
